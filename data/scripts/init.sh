1184b5cd6773bf8fd106419553e1282720fa5478
Tobi Okedeji <tobiokedeji@gmail.com>
2024-08-06 22:06:12 +0100

    fix issues with docker-compose initialize

--- data/scripts/init.sh
+++ data/scripts/init.sh
@@ -6,7 +6,6 @@ NAME="${NAME:-offchain_node}"
 
 if allorad keys --home=/data/.allorad --keyring-backend test show $NAME > /dev/null 2>&1 ; then
     echo "allora account: $NAME already imported"
-    echo "ENV_LOADED=true" >> /data/env_file
 else
     echo "creating allora account: $NAME"
     output=$(allorad keys add $NAME --home=/data/.allorad --keyring-backend test 2>&1)
@@ -25,9 +24,13 @@ else
     echo "ALLORA_OFFCHAIN_NODE_CONFIG_JSON='$stringified_json'" > /data/env_file
     echo ALLORA_OFFCHAIN_ACCOUNT_ADDRESS=$address >> /data/env_file
     echo "NAME=$NAME" >> /data/env_file
-    echo "ENV_LOADED=true" >> /data/env_file
-
-    
 
     echo "Updated ALLORA_OFFCHAIN_NODE_CONFIG_JSON saved to /data/env_file"
+fi
+
+
+if grep -q "ENV_LOADED=false" /data/env_file; then
+    sed -i 's/ENV_LOADED=false/ENV_LOADED=true/' /data/env_file
+else
+    echo "ENV_LOADED=true" >> /data/env_file
 fi
--- data/scripts/reload-node.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Reload environment variables from the env_file
-export $(grep -v '^#' /data/env_file | xargs)
-
-# Start the main application
-exec "$@"
