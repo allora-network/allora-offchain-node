37c98aca51854661bc7dc74bc99124e9f0c46d00
Diego Campo <xmariachi@gmail.com>
2025-01-16 17:22:55 +0100

    expand range on numerically expressed cosmosInt

--- lib/type_allora.go
+++ lib/type_allora.go
@@ -18,18 +18,21 @@ type FlexibleCosmosIntAmount struct {
 }
 
 func (fv *FlexibleCosmosIntAmount) String() string {
-	fmt.Println("fv.Number", fv.Number)
 	return fv.Number.String()
 }
 
 func (fv *FlexibleCosmosIntAmount) UnmarshalJSON(data []byte) error {
 	// Handle number
 	if data[0] >= '0' && data[0] <= '9' {
-		var num int64
+		var num json.Number
 		if err := json.Unmarshal(data, &num); err != nil {
 			return fmt.Errorf("failed to parse number: %w", err)
 		}
-		fv.Number = cosmossdk_io_math.NewInt(num)
+		cosmosInt, ok := cosmossdk_io_math.NewIntFromString(num.String())
+		if !ok {
+			return fmt.Errorf("failed to convert number to cosmosInt: %s", num.String())
+		}
+		fv.Number = cosmosInt
 		return nil
 	}
 
