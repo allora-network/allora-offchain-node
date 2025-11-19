27468b23ce1925f81b3cba4e466eb37cfcb0b027
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2025-11-19 15:29:25 +0100

    feat: introduce window waker component

---
+++ lib/window_waker.go
@@ -0,0 +1,286 @@
+package lib
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+
+	emissionstypes "github.com/allora-network/allora-chain/x/emissions/types"
+	cometrpc "github.com/cometbft/cometbft/rpc/client/http"
+	coretypes "github.com/cometbft/cometbft/rpc/core/types"
+	comettypes "github.com/cometbft/cometbft/types"
+	cosmostypes "github.com/cosmos/cosmos-sdk/types"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"google.golang.org/grpc/metadata"
+)
+
+type WindowWakeFn func(context.Context, emissionstypes.Nonce, int64)
+
+type WindowType int
+
+const (
+	ReputerWindow WindowType = iota
+	WorkerWindow
+)
+
+type WindowWaker struct {
+	windowType        WindowType
+	connectionManager ConnectionManagerInterface
+	logger            zerolog.Logger
+	topicID           emissionstypes.TopicId
+	wakeFn            WindowWakeFn
+
+	wsNode   *cometrpc.HTTP
+	wsQuery  string
+	wsWG     *sync.WaitGroup
+	wsStopCh chan struct{}
+}
+
+func NewReputerWindowWaker(
+	connectionManager ConnectionManagerInterface,
+	topicID emissionstypes.TopicId,
+	wakeFn WindowWakeFn,
+) *WindowWaker {
+	return &WindowWaker{
+		windowType:        ReputerWindow,
+		connectionManager: connectionManager,
+		logger:            log.With().Uint64("topicId", topicID).Int("windowType", int(ReputerWindow)).Logger(),
+		topicID:           topicID,
+		wakeFn:            wakeFn,
+	}
+}
+
+func NewWorkerWindowWaker(
+	connectionManager ConnectionManagerInterface,
+	topicID emissionstypes.TopicId,
+	wakeFn WindowWakeFn,
+) *WindowWaker {
+	return &WindowWaker{
+		windowType:        WorkerWindow,
+		connectionManager: connectionManager,
+		logger:            log.With().Uint64("topicId", topicID).Int("windowType", int(WorkerWindow)).Logger(),
+		topicID:           topicID,
+		wakeFn:            wakeFn,
+	}
+}
+
+func (ww *WindowWaker) Start(ctx context.Context) error {
+	ww.logger.Info().Msg("Starting window waker")
+
+	latestBlock, err := RunWithNodeRetry(
+		ctx,
+		ww.connectionManager,
+		func(node *NodeConfig) (*coretypes.ResultBlock, error) {
+			return node.Chain.RPCClient.Client.Block(ctx, nil)
+		},
+		"get latest block",
+		RPC_MODE,
+	)
+	if err != nil {
+		return err
+	}
+	latestHeight := latestBlock.Block.Height
+
+	if err := ww.subscribeToWindowEvents(ctx); err != nil {
+		return err
+	}
+
+	nonce, err := ww.getOpenNonceAtHeight(ctx, latestHeight)
+	if err != nil {
+		return err
+	}
+	if nonce != nil {
+		ww.logger.Info().Int64("nonce", *nonce).Msg("Currently opened window")
+		ww.wakeFn(ctx, emissionstypes.Nonce{BlockHeight: *nonce}, latestHeight)
+	}
+
+	return nil
+}
+
+func (ww *WindowWaker) subscribeToWindowEvents(ctx context.Context) error {
+	node, err := ww.connectionManager.GetCurrentTxNode()
+	if err != nil {
+		return err
+	}
+
+	ww.wsNode = node.Chain.RPCClient.Client
+	evtsChan, err := ww.wsNode.Subscribe(ctx, "", ww.prepareWSQuery())
+	if err != nil {
+		return err
+	}
+	ww.wsStopCh = make(chan struct{})
+	ww.wsWG = &sync.WaitGroup{}
+
+	ww.wsWG.Add(1)
+	go func() {
+		defer ww.wsWG.Done()
+
+		for {
+			select {
+			case <-ww.wsStopCh:
+				ww.logger.Info().Msg("Stopping window waker")
+				return
+			case event := <-evtsChan:
+				blockEvts, ok := event.Data.(comettypes.EventDataNewBlockEvents)
+				if !ok {
+					ww.logger.Info().Msg("Received non comettypes.EventDataNewBlockEvents from comet ws")
+					continue
+				}
+
+				var nonce *int64
+				for _, abciEvt := range blockEvts.Events {
+					// HACK: Remove comet 'mode' additional attribute
+					for i, attribute := range abciEvt.Attributes {
+						if attribute.Key == "mode" {
+							abciEvt.Attributes = append(abciEvt.Attributes[:i], abciEvt.Attributes[i+1:]...)
+							break
+						}
+					}
+
+					evt, err := cosmostypes.ParseTypedEvent(abciEvt)
+					if err != nil {
+						continue
+					}
+
+					if ww.windowType == ReputerWindow {
+						windowEvt, ok := evt.(*emissionstypes.EventReputerSubmissionWindowOpened)
+						if ok && windowEvt.TopicId == ww.topicID {
+							nonce = &windowEvt.NonceBlockHeight
+							break
+						}
+					} else {
+						windowEvt, ok := evt.(*emissionstypes.EventWorkerSubmissionWindowOpened)
+						if ok && windowEvt.TopicId == ww.topicID {
+							nonce = &windowEvt.NonceBlockHeight
+							break
+						}
+					}
+				}
+				if nonce != nil {
+					ww.logger.Info().Int64("nonce", *nonce).Msg("New window open event")
+					ww.wakeFn(ctx, emissionstypes.Nonce{BlockHeight: *nonce}, blockEvts.Height)
+				} else {
+					ww.logger.Error().Msg("Couldn't lookup window event in block events")
+				}
+			}
+		}
+	}()
+
+	return nil
+}
+
+func (ww *WindowWaker) getOpenNonceAtHeight(ctx context.Context, height int64) (*int64, error) {
+	heightCtx := metadata.AppendToOutgoingContext(ctx, "x-cosmos-block-height", fmt.Sprintf("%d", height))
+	if ww.windowType == WorkerWindow {
+		window, err := RunWithNodeRetry(
+			ctx,
+			ww.connectionManager,
+			func(node *NodeConfig) (*emissionstypes.GetWorkerSubmissionWindowStatusResponse, error) {
+				return emissionstypes.NewQueryServiceClient(node.Chain.GRPCClient).
+					GetWorkerSubmissionWindowStatus(
+						heightCtx,
+						&emissionstypes.GetWorkerSubmissionWindowStatusRequest{TopicId: ww.topicID},
+					)
+			},
+			"get current window status",
+			GRPC_MODE,
+		)
+		if err != nil {
+			return nil, err
+		}
+		if window.IsOpen {
+			return &window.CurrentNonceBlockHeight, nil
+		}
+	} else {
+		window, err := RunWithNodeRetry(
+			ctx,
+			ww.connectionManager,
+			func(node *NodeConfig) (*emissionstypes.GetReputerSubmissionWindowStatusResponse, error) {
+				return emissionstypes.NewQueryServiceClient(node.Chain.GRPCClient).
+					GetReputerSubmissionWindowStatus(
+						heightCtx,
+						&emissionstypes.GetReputerSubmissionWindowStatusRequest{TopicId: ww.topicID},
+					)
+			},
+			"get current window status",
+			GRPC_MODE,
+		)
+		if err != nil {
+			return nil, err
+		}
+		if window.IsOpen {
+			return &window.CurrentNonceBlockHeight, nil
+		}
+	}
+	return nil, nil
+}
+
+func (ww *WindowWaker) prepareWSQuery() string {
+	var qFormat string
+	if ww.windowType == ReputerWindow {
+		qFormat = "tm.event='NewBlockEvents' AND emissions.v9.EventReputerSubmissionWindowOpened.topic_id='\"%d\"'"
+	} else {
+		qFormat = "tm.event='NewBlockEvents' AND emissions.v9.EventWorkerSubmissionWindowOpened.topic_id='\"%d\"'"
+	}
+	ww.wsQuery = fmt.Sprintf(qFormat, ww.topicID)
+
+	return ww.wsQuery
+}
+
+func (ww *WindowWaker) Stop(ctx context.Context) {
+	ww.wsStopCh <- struct{}{}
+	if err := ww.wsNode.Unsubscribe(ctx, "", ww.wsQuery); err != nil {
+		ww.logger.Err(err).Msg("Failed to unsubscribe comet ws stopping window waker")
+	}
+
+	ww.wsWG.Wait()
+}
+
+type SubmissionWindowManager struct {
+	connectionManager ConnectionManagerInterface
+	wakers            []*WindowWaker
+}
+
+func NewSubmissionWindowManager(connectionManager ConnectionManagerInterface) *SubmissionWindowManager {
+	return &SubmissionWindowManager{
+		connectionManager: connectionManager,
+	}
+}
+
+func (swm *SubmissionWindowManager) WakeOnWorkerWindowOpen(ctx context.Context, topicID emissionstypes.TopicId, wakeFn WindowWakeFn) error {
+	ww := NewWorkerWindowWaker(swm.connectionManager, topicID, wakeFn)
+	if err := ww.Start(ctx); err != nil {
+		return err
+	}
+
+	swm.wakers = append(swm.wakers, ww)
+	return nil
+}
+
+func (swm *SubmissionWindowManager) WakeOnReputerWindowOpen(ctx context.Context, topicID emissionstypes.TopicId, wakeFn WindowWakeFn) error {
+	ww := NewReputerWindowWaker(swm.connectionManager, topicID, wakeFn)
+	if err := ww.Start(ctx); err != nil {
+		return err
+	}
+
+	swm.wakers = append(swm.wakers, ww)
+	return nil
+}
+
+func (swm *SubmissionWindowManager) Stop() {
+	ctx, cl := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cl()
+
+	var wg sync.WaitGroup
+	for _, w := range swm.wakers {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			w.Stop(ctx)
+		}()
+	}
+
+	wg.Wait()
+}
